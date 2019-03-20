a12b11856787d67f08d86d6d353348092c212354
disneyharley26 <disneyharley26@gmail.com>
2019-03-20 15:23:11 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -6,6 +6,4 @@ var greetSpecialGuest = "Hello, " + specialGuest + "!";
 
 var topic = "space";
 
-var conversation = `${topic}`;
-
-console.log(`Let's talk about ${topic}.`);
+var conversation = `Let's talk about space ${topic}`;
